cdd2e58da485914be6a9913502a393faec819e1c
Josh Regondola <josh@klab.com>
2013-08-04 13:45:23 +0800

    Revert "First commit"
    This reverts commit 71f3952500dd0e2e7cbf6f07cebc9c530bafbd28.

--- cashman/src/com/rego/cashman/model/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.rego.cashman.model;
-
-public class Constant {
-
-	public static final String PREF_KEY = "cashman";
-	public static final String PREF_USER = "user";
-	public static final String PREF_CASH = "cash";
-	
-}
